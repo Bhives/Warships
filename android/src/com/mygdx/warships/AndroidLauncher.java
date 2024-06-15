43f9a8359ad16913ce730dee86db660824fd307a
Bhives <scroush@yandex.ru>
2024-06-15 14:05:42 +0300

    Init

---
+++ android/src/com/mygdx/warships/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.warships;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.warships.Warships;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Warships(), config);
+	}
+}
