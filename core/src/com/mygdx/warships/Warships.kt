a589a722544014e90a6e32236ab5d148dcda4abf
Bhives <scroush@yandex.ru>
2024-06-21 19:02:23 +0300

    Adjusted grid render, added text drawing logic

--- core/src/com/mygdx/warships/Warships.kt
+++ core/src/com/mygdx/warships/Warships.kt
@@ -9,6 +9,12 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Button
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.mygdx.warships.Assets.battleshipTexture
 import com.mygdx.warships.Assets.carrierTexture
 import com.mygdx.warships.Assets.cruiserTexture
@@ -27,9 +33,14 @@ class Warships : ApplicationAdapter() {
     private lateinit var battleshipSprite: Sprite
     private lateinit var carrierSprite: Sprite
     private lateinit var field: ShapeRenderer
+
+    private lateinit var stage: Stage
+    private lateinit var button: Button
+
     private var startPoint = 0f
     private var cellSize = 0f
     private var fieldLength = 0f
+    private val coordinatePoints = listOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
 
     private val ships = mutableListOf<Ship>()
 
@@ -42,6 +53,9 @@ class Warships : ApplicationAdapter() {
         font = BitmapFont()
         spriteBatch = SpriteBatch()
 
+        stage = Stage()
+        Gdx.input.inputProcessor = stage
+
         Assets.load()
 
         destroyerSprite = Sprite(destroyerTexture)
@@ -50,6 +64,8 @@ class Warships : ApplicationAdapter() {
         carrierSprite = Sprite(carrierTexture)
 
         positionShips()
+
+        drawButton()
     }
 
     override fun render() {
@@ -60,15 +76,44 @@ class Warships : ApplicationAdapter() {
         field.setProjectionMatrix(camera.combined)
         spriteBatch.setProjectionMatrix(camera.combined)
 
+        stage.act(Gdx.graphics.deltaTime)
+        stage.draw()
+
         renderGrid()
         ships.forEach { ship ->
             renderShip(ship)
         }
     }
 
+    override fun dispose() {
+        field.dispose()
+        Assets.dispose()
+        stage.dispose()
+        spriteBatch.dispose()
+    }
+
+    private fun drawButton() {
+        val skin = Skin(Gdx.files.internal(SKIN_PATH))
+        button = TextButton(AUTO_TEXT, skin)
+        button.setPosition(
+            Gdx.graphics.width / BUTTON_HORIZONTAL_MARGIN_RATIO - button.width / BUTTON_MARGIN_RATIO,
+            Gdx.graphics.height / BUTTON_MARGIN_RATIO - button.height / BUTTON_MARGIN_RATIO
+        )
+
+        button.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                positionShips()
+            }
+        })
+
+        stage.addActor(button)
+    }
+
     private fun positionShips() {
+        ships.clear()
+        filledCells.clear()
         //destroyers
-        for (i in 0 until 4) {
+        for (i in 0 until DESTROYERS_NUMBER) {
             positionShip(
                 getRandomPosition(),
                 getRandomPosition(),
@@ -79,7 +124,7 @@ class Warships : ApplicationAdapter() {
         }
 
         //cruisers
-        for (i in 0 until 3) {
+        for (i in 0 until CRUISERS_NUMBER) {
             positionShip(
                 getRandomPosition(),
                 getRandomPosition(),
@@ -90,7 +135,7 @@ class Warships : ApplicationAdapter() {
         }
 
         //battleships
-        for (i in 0 until 2) {
+        for (i in 0 until BATTLESHIPS_NUMBER) {
             positionShip(
                 getRandomPosition(),
                 getRandomPosition(),
@@ -174,25 +219,31 @@ class Warships : ApplicationAdapter() {
 
     private fun renderGrid() {
         field.begin(ShapeRenderer.ShapeType.Line)
+        spriteBatch.begin()
 
         field.color = Color.WHITE
 
         fieldLength = Gdx.graphics.height / GRID_HEIGHT_RATIO
         cellSize = fieldLength / GRID_SIZE
         val screenMargin = Gdx.graphics.height / GRID_MARGIN_RATIO
-        startPoint = (Gdx.graphics.height - screenMargin)
+        startPoint = Gdx.graphics.height - screenMargin
 
-        for (i in cellSize.toInt()..fieldLength.toInt() step cellSize.toInt()) {
-            field.line(i.toFloat(), startPoint, i.toFloat(), fieldLength)
-            field.line(startPoint, i.toFloat(), fieldLength, i.toFloat())
+        for (i in MIN_COORDINATE..GRID_SIZE) {
+            field.line(cellSize * i, cellSize, cellSize * i, fieldLength)
+            field.line(cellSize, cellSize * i, fieldLength, cellSize * i)
+        }
+
+        for (i in MIN_COORDINATE..MAX_COORDINATE) {
+            font.draw(spriteBatch, coordinatePoints[i - 1], cellSize * (i), cellSize * (i))
         }
 
         destroyerSprite.setSize(cellSize, cellSize)
-        cruiserSprite.setSize(cellSize, cellSize * 2)
-        battleshipSprite.setSize(cellSize, cellSize * 3)
-        carrierSprite.setSize(cellSize, cellSize * 4)
+        cruiserSprite.setSize(cellSize, cellSize * CRUISER_SIZE)
+        battleshipSprite.setSize(cellSize, cellSize * BATTLESHIP_SIZE)
+        carrierSprite.setSize(cellSize, cellSize * CARRIER_SIZE)
 
         field.end()
+        spriteBatch.end()
     }
 
     private fun renderShip(ship: Ship) {
@@ -227,21 +278,26 @@ class Warships : ApplicationAdapter() {
         return Random().nextBoolean()
     }
 
-    override fun dispose() {
-        field.dispose()
-        Assets.dispose()
-    }
-
     companion object {
         private const val GRID_SIZE = 11
-        private const val GRID_HEIGHT_RATIO = 1.2f
-        private const val GRID_MARGIN_RATIO = 1.1f
+        private const val MIN_COORDINATE = 1
         private const val MAX_COORDINATE = 10
+        private const val GRID_HEIGHT_RATIO = 1.1f
+        private const val GRID_MARGIN_RATIO = 1.1f
         private const val ROTATION = 270f
 
+        private const val BUTTON_HORIZONTAL_MARGIN_RATIO = 1.5f
+        private const val BUTTON_MARGIN_RATIO = 2f
+
+        private const val DESTROYERS_NUMBER = 4
+        private const val CRUISERS_NUMBER = 3
+        private const val BATTLESHIPS_NUMBER = 2
         private const val DESTROYER_SIZE = 1
         private const val CRUISER_SIZE = 2
         private const val BATTLESHIP_SIZE = 3
         private const val CARRIER_SIZE = 4
+
+        private const val SKIN_PATH = "skin/glassy-ui.json"
+        private const val AUTO_TEXT = "Auto"
     }
 }
